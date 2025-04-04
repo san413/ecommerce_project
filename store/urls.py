57fabf770fed8dc883ce719d6d22ed6beaad0155
san413 <sanjayprajapati40458@gmai.com>
2025-04-04 10:25:28 +0530

    Added link button home, login, cart

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/', views.products_view, name='products'),
+    path('cart/', views.cart_view, name='cart'),
+    path('login/', views.login_view, name='login'),
 ]
--- store/views.py
+++ store/views.py
@@ -4,3 +4,13 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'store/products.html', {'products': products})
+
+def cart_view(request):
+    return render(request, 'store/cart.html')
+
+def login_view(request):
+    return render(request, 'store/login.html')
